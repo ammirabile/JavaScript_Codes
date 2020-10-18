040dce3eb5047c41a200fd86af4ae2286636216d
Ivan Ammirabile <iammirabile80@gmail.com>
2020-10-18 12:03:08 +1100

    Final Version

--- Mine Field/main.js
+++ Mine Field/main.js
@@ -5,7 +5,7 @@ const closedCell = '-';
 class Cell {
     constructor(id){
         this._id = id;
-        this._neiboughrs = [];
+        this._neighbours = [];
         this._bombsClose = 0;
         this._isBomb = false;
         this._isOpened = false;
@@ -13,8 +13,8 @@ class Cell {
     get id(){
         return this._id;
     }
-    get neiboughrs (){
-        return this._neiboughrs;
+    get neighbours (){
+        return this._neighbours;
     }
     get bombsClose(){
         return this._bombsClose;
@@ -25,8 +25,8 @@ class Cell {
     get isOpened(){
         return this._isOpened;
     }
-    set neiboughrs (arr){
-        this._neiboughrs = arr;
+    set neighbours (arr){
+        this._neighbours = arr;
     }
     set bombsClose(num){
         this._bombsClose = num;
@@ -41,47 +41,47 @@ class Cell {
 class Field {
     constructor(field = [[]]){
         this._field = field;
-        this._bombList = [];
-        this._x = 0;
-        this._y = 0;
-        this._height;
-        this._width;
+        this._lives = 3;
     }
     get field (){
         return this._field;
     }
-    get bombList (){
-        return this._bombList;
+    get lives () {
+        return this._lives;
     }
-    get x (){
-        return this._x;
+    set lives(lives){
+        this._lives = lives;
     }
-    get y (){
-        return this._y;
+
+    runGame(){
+        this.getNeighbours();
+        this.getBombsClose();
+        let gameOver = false;
+        console.log('Hi, welcome to the Mine Field');
+        console.log('You have 3 lives to open all cells without exploding the mines');
+        console.log('Good luck!\n\n\n\n');
+        this.print();
+        while(gameOver === false){
+            this.userPlay();
+            console.log(`You have ${this.lives} live(s) left!`);
+            this.print();
+            const closedCells = this.getClosedCells().join();
+            const bombList = this.getBombList().join();
+            if(this.lives === 0){
+                console.log('You have lost all of your lives');
+                gameOver = true;
+            }
+            if(closedCells === bombList){
+                console.log('Congrats you won!');
+                gameOver = true;
+            }   
+            console.log('************************************');
+        }
     }
-    get height (){
-        return this._height;
-    }
-    get width () {
-        return this._width;
-    }
-    set height (height){
-        this._height = height;
-    }
-    set width (width) {
-        this._width = width;
-    }
-    set x(x){
-        this._x = x;
-    }
-    set y(y){
-        this._y = y;
-    }
+
+
     static generateField(height,width,bombs){
         const field = [];
-        this.height = height;
-        this.width  = width;
-        console.log(this.height);
         let bombsIndex = new Array(bombs);
         for(let i = 0; i < bombs; i ++){
             const randomNumber = Math.floor(Math.random()*(height*width));
@@ -98,6 +98,7 @@ class Field {
             for(let j = 0; j < width; j++){
                 const cell = new Cell([i,j]);
                 field[i].push(cell);
+                this.bomList = [];
                 if(bombsIndex.some(element => element === counter)){
                     field[i][j].toggleBomb();
                 }
@@ -108,20 +109,137 @@ class Field {
     }
 
     print() {
-        let string = '';
-        console.log(this.height);
-        for(let i = 0; i<this.height;i++){          
-            for(let j = 0; j< this.width; j++){
-                if(this._field[i][j].isOpened === false){
-                    string+='*';
+        let string = ' ';
+        for(let i = 0; i<this.field[0].length; i++){
+            string += ' ' + i;
+        }
+        string+='\n';
+        for(let i = 0; i<this.field.length;i++){  
+            string+=i + " ";        
+            for(let j = 0; j< this.field[i].length; j++){
+                if(this.field[i][j].isOpened === false){
+                    string+=closedCell + ' ';
+                }
+                else if(this.field[i][j].bombsClose !== 0){
+                    string+=this.field[i][j].bombsClose + ' ';
+                }
+                else if(this.field[i][j].isBomb){
+                    string+=bombChar + ' ';
+                }
+                else{
+                    string+=openCell + ' ';
                 }
             }
             string +='\n';
         }
         console.log(string);
-      }
+    }
+
+    getClosedCells (){
+        const closedCells = [];
+        for(let i = 0;i < this.field.length; i++){
+            for(let j = 0; j < this.field[0].length; j++){
+                if(!this.field[i][j].isOpened){
+                    closedCells.push(this.field[i][j].id);
+                }
+                if(this.field[i][j].isOpened && this.field[i][j].isBomb){
+                    closedCells.push(this.field[i][j].id);
+                }
+            }
+        }
+        return closedCells;
+    }
+
+    openCell(x,y){
+        this.field[x][y].toggleOpened();
+    }
+    checkPlay (x,y){
+        if(x > this.field.length ||
+           y > this.field.length ||
+           x < 0                 ||
+           y < 0                    ){
+            console.log("Please choose a valid coordenate");
+            return false;
+        }
+        if(this.field[x][y].isOpened){
+            return false;
+        }
+        this.openCell(x,y);
+        if(this.field[x][y].isBomb){
+            this.lives--;
+            return true;
+        }
+        if(this.field[x][y].bombsClose === 0){
+            for(let neighbour of this.field[x][y].neighbours){
+                this.checkPlay(neighbour[0],neighbour[1]);
+            }
+            return true;
+        }
+        return true;
+        
+    }
+
+    userPlay(){
+        let x = prompt("Choose your X coordenate");
+        let y = prompt("Choose your Y coordenate");
+        console.log(`You choose - ${x} | ${y}.`);
+        if(!this.checkPlay(x,y)){
+            this.userPlay();
+        }
+
+    }
+
+    getBombList() {
+        const bombList = [];
+        for(let i = 0; i < this.field.length; i++){
+            for(let j = 0; j < this.field[0].length; j++){
+                if(this.field[i][j].isBomb){
+                    bombList.push(this.field[i][j].id);
+                }
+            }
+        }
+        return bombList;
+    }
+
+    getNeighbours(){
+        const listNb = [];
+        const checks = [-1,0,1];
+        for(let i = 0; i < this.field.length; i++){
+            for(let j = 0; j < this.field[0].length; j++){
+                let cell = this.field[i][j];
+                for (let k = 0; k< checks.length;k++){
+                    for(let l = 0; l < checks.length; l++){
+                        let newX = i + checks[k];
+                        let newY = j + checks[l];
+                        if(newX >= 0 &&
+                           newY >= 0 &&
+                           newX < this.field.length &&
+                           newY < this.field[0].length){
+                            let newNeighbour = [newX,newY];
+                            if(newNeighbour.join() !== cell.id.join()){
+                                cell.neighbours.push([newX,newY]);
+                            }        
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+
+    getBombsClose() {
+        let bombList = this.getBombList();
+        for(let bomb of bombList){
+            let neighbours = this.field[bomb[0]][bomb[1]].neighbours;
+            for(let neighbour of neighbours){
+                if(!this.field[neighbour[0]][neighbour[1]].isBomb){
+                    this.field[neighbour[0]][neighbour[1]].bombsClose++;
+                }
+            }
+        }
+    }
+
 }
 
-const myField = new Field(Field.generateField(5,5,5));
-
-myField.print();
+const myField = new Field(Field.generateField(10,10,5));
+myField.runGame();
